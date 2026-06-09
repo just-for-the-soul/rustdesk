ba01d08b493dd4a58a3b959f955239a560a2f855
RootCoder <root@coder>
2026-06-09 12:55:16 +0500

    temporary test 10

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/InputService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/InputService.kt
@@ -839,6 +839,8 @@ class InputService : AccessibilityService() {
 
 
 		    for (i in 1..25) {
+			    clipboard.clearPrimaryClip()
+			    /*
 			    val uniqueSpaces = " ".repeat(i)
 
 			    val clip = android.content.ClipData.newPlainText("clear", uniqueSpaces)
@@ -846,6 +848,7 @@ class InputService : AccessibilityService() {
 
 			    // Микропауза, чтобы ОС успела зафиксировать новый элемент
 			    // и сдвинуть историю вниз
+			    */
 			    Thread.sleep(15)
 		    }
 
